22a023df896962bb8c1d175036779abc3cd16be2
felipemaxplay <felipemaxplay@gmail.com>
2022-07-03 19:32:59 -0300

    first template

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,20 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btnCalculate_clicked()
+{
+    QString result;
+    int firstCount, finalCount;
+    int frameTarget = ui ->leFrameTarget -> text().toInt();
+    int frameInit = ui ->leFrameInit -> text().toInt();
+    int frameHit = ui ->leFrameHit -> text().toInt();
+
+    firstCount = frameTarget - frameHit;
+    finalCount = firstCount + frameInit;
+
+    result = QString::number(finalCount);
+
+    ui -> lbResultCalibration -> setText(result);
+}
+
